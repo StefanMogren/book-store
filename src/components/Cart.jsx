7d03d298928a535cdfc3e229e9e75dc475fef443
StefanMogren <stefan_mogren@hotmail.com>
2025-03-27 09:49:34 +0100

    Created components for header

---
+++ src/components/Cart.jsx
@@ -0,0 +1,10 @@
+function Cart() {
+	return (
+		<div className='header__cart'>
+			Cart
+			<button className='header__cart-button'></button>
+		</div>
+	);
+}
+
+export default Cart;
---
+++ src/components/Logo.jsx
@@ -0,0 +1,8 @@
+function Logo() {
+	return (
+		<div>Logo</div>
+		// <img src="" alt="" />
+	);
+}
+
+export default Logo;
