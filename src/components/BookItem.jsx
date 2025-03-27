125f9dd5757c6650da469446daed8e67cdbe94c2
StefanMogren <stefan_mogren@hotmail.com>
2025-03-27 12:04:29 +0100

    Adjusted CSS of button and title, added text to author section

--- src/components/BookItem.jsx
+++ src/components/BookItem.jsx
@@ -2,7 +2,7 @@ function BookItem(props) {
 	return (
 		<li className='main__book-item'>
 			<h2 className='main__book-title'>{props.title}</h2>
-			<h3 className='main__book-author'>{props.author}</h3>
+			<h3 className='main__book-author'>Av {props.author}</h3>
 			<p className='main__book-description'>{props.description}</p>
 			<button className='main__book-add-cart-button'>Add to cart</button>
 		</li>
