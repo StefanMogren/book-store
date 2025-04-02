7814b18394e746b69ef3ae0c9dc540accd6f5e24
StefanMogren <stefan_mogren@hotmail.com>
2025-04-02 14:24:24 +0200

    Created Input.jsx

---
+++ src/components/Input.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Input({
+	inputType,
+	inputName,
+	inputClass,
+	inputId,
+	inputValue,
+	inputRequired,
+	labelClass,
+	labelText,
+	spanClass,
+}) {
+	return (
+		<label className={labelClass} htmlFor={inputId}>
+			<span className={spanClass}>{labelText}</span>
+			<input
+				type={inputType}
+				className={inputClass}
+				id={inputId}
+				name={inputName}
+				required={inputRequired}
+				value={inputValue}
+			/>
+		</label>
+	);
+}
+
+export default Input;
